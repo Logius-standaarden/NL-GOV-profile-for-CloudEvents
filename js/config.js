a671ec69bbce1a8c60fe2c844a35ceb203601bbd
Alexander Green <alexander.green@logius.nl>
2024-07-16 23:40:05 +0200

    Revert "logius.nl hoofdlettergevoelig"
    This reverts commit 4531226e86b4094c33a4cad8edb463be0c4d8104.

--- js/config.js
+++ js/config.js
@@ -31,7 +31,7 @@ var respecConfig = {
   previousPublishVersion: "0.3",
   publishDate: "2024-06-27",
   publishVersion: "1.0",
-  shortName: "cloudevents-nl",
+  shortName: "CloudEvents-NL",
   specStatus: "WV",
   specType: "ST",
   subtitle: "Notificatieservices"
