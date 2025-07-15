3a517e915046b9e5659143b34041a0ce97ae4430
Tim van der Lippe <Tim.Lippe@logius.nl>
2025-07-15 14:07:12 +0200

    Beheer: gebruik nieuwe organisation-config setup
    Part of Logius-standaarden/Automatisering#51

---
+++ js/config.mjs
@@ -0,0 +1,50 @@
+import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+
+loadRespecWithConfiguration({
+  alternateFormats: [{
+    "label": "pdf",
+    "uri": "CloudEvents-NL.pdf"
+  }],
+  authors: [{
+    "company": "Project Notificatieservices",
+    "name": "Werkgroep Berichtenstandaard"
+  }],
+  editors: [
+    {
+      "company": "VNG Realisatie",
+      "companyURL": "https://www.vngrealisatie.nl/",
+      "name": "Gershon Jansen"
+    },
+    {
+      "company": "VNG Realisatie",
+      "companyURL": "https://www.vngrealisatie.nl/",
+      "name": "Ad Gerrits"
+    },
+    {
+      "company": "Logius",
+      "name": "Edwin Wisse",
+      "url": "https://logius.nl/standaarden"
+    },
+    {
+      "company": "Logius",
+      "name": "Alexander Green",
+      "url": "https://logius.nl/standaarden"
+    },
+    {
+      "company": "Logius",
+      "name": "Stas Mironov",
+      "url": "https://logius.nl/standaarden"
+    }
+  ],
+  github: "https://github.com/Logius-standaarden/NL-GOV-profile-for-CloudEvents",
+  previousMaturity: "WV",
+  pubDomain: "notificatieservices",
+  previousPublishDate: "2024-06-06",
+  previousPublishVersion: "0.3",
+  publishDate: "2024-06-27",
+  publishVersion: "1.0",
+  shortName: "CloudEvents-NL",
+  specStatus: "WV",
+  specType: "ST",
+  subtitle: "Notificatieservices"
+});
