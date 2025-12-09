e7c5c0fd45c735d8cc76b51dea134b2af18fbc48
Stas Mironov <stas.mironov@logius.nl>
2025-12-09 11:27:57 +0100

    Aanpassen editors

--- js/config.mjs
+++ js/config.mjs
@@ -1,36 +1,33 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  authors: [{
-    "company": "Project Notificatieservices",
-    "name": "Werkgroep Berichtenstandaard"
-  }],
-  editors: [
-    {
-      "company": "VNG Realisatie",
-      "companyURL": "https://www.vngrealisatie.nl/",
-      "name": "Gershon Jansen"
-    },
-    {
-      "company": "VNG Realisatie",
-      "companyURL": "https://www.vngrealisatie.nl/",
-      "name": "Ad Gerrits"
-    },
-    {
-      "company": "Logius",
-      "name": "Edwin Wisse",
-      "url": "https://logius.nl/standaarden"
-    },
-    {
-      "company": "Logius",
-      "name": "Alexander Green",
-      "url": "https://logius.nl/standaarden"
-    },
-    {
-      "company": "Logius",
-      "name": "Stas Mironov",
-      "url": "https://logius.nl/standaarden"
-    }
+  authors: [{ 
+        "company" : "VNG Realisatie",
+        "companyURL" : "https://www.vngrealisatie.nl/",
+        "name" : "Ad Gerrits"
+      },
+      { 
+        "company" : "VNG Realisatie",
+        "companyURL" : "https://www.vngrealisatie.nl/",
+        "name" : "Gershon Jansen"
+      },
+      { 
+        "company" : "VNG Realisatie",
+        "companyURL" : "https://www.vngrealisatie.nl/",
+        "name" : "Jeanot Bijpost"
+      }
+           ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Alexander Green",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Stas Mironov",
+        "url" : "https://logius.nl/standaarden"
+      }
   ],
   github: "https://github.com/Logius-standaarden/NL-GOV-profile-for-CloudEvents",
   previousMaturity: "WV",
